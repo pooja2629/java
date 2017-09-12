de852e723c4dba60085c099c77f46fc81c2fba6d
pooja2629 <poojaborhade29@yahoo.in>
2017-09-12 12:22:35 +0530

    first commit

---
+++ ConditionalDemo.java
@@ -0,0 +1,36 @@
+package variablesAndData;
+
+import java.util.Scanner;
+
+public class ConditionalDemo {
+
+    void conditionCheck()
+    {
+        Scanner scanner= new Scanner(System.in);
+         System.out.println("enter the value of a:");
+         int a =scanner.nextInt();
+
+        if (a<35)
+        {
+            System.out.println("fail");
+        }
+          else if (35<=a && a<60)
+        {
+            System.out.println("pass");
+        }
+        else if (60<=a && a<75)
+        {
+            System.out.println("first class");
+        }
+        else if (75<=a && a<100)
+        {
+            System.out.println("distinction");
+        }
+    }
+
+    public static void main(String[] args) {
+        ConditionalDemo conditionalDemo=new ConditionalDemo();
+        conditionalDemo.conditionCheck();
+    }
+
+}
